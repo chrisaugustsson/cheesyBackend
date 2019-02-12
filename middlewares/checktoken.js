56f65d40819e6cb842bca50a3e1eb8143f76f22a
unknown <you@example.com>
2019-02-12 20:51:07 +0000

    Fixed bug where app crashes if Token is invalid

--- middlewares/checktoken.js
+++ middlewares/checktoken.js
@@ -4,9 +4,9 @@ module.exports = function (req, res, next) {
     const token = req.headers['x-access-token'];
     const secret = process.env.JWT_SECRET || "secret";
 
-    jwt.verify(token, secret, function(err, decoded) {
+    jwt.verify(token, secret, function (err, decoded) {
         if (err) {
-            res.status(401).json({
+            return res.status(401).json({
                 error: "Token not recognized",
                 details: err
             });
