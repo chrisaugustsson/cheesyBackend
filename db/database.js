9147bac11eb4f2129793b00b323b596cc7be3f6d
unknown <you@example.com>
2019-02-17 17:49:59 +0000

    Travis problems

--- db/database.js
+++ db/database.js
@@ -2,6 +2,7 @@ var sqlite3 = require('sqlite3');
 
 module.exports = (function () {
     if (process.env.NODE_ENV === 'test') {
+        console.log("Running in test");
         return new sqlite3.Database('./db/test.sqlite');
     }
     return new sqlite3.Database('./db/texts.sqlite');
